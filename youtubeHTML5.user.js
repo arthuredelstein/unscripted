86634f69eeeffc2087ce975cc708551a17995055
Arthur Edelstein <arthuredelstein@gmail.com>
2014-02-01 20:18:24 -0800

    update documentation

--- youtubeHTML5.user.js
+++ youtubeHTML5.user.js
@@ -2,8 +2,8 @@
 
 // by Arthur Edelstein, 2014. [BSD 3-Clause License.](http://opensource.org/licenses/BSD-3-Clause)
 
-// This is a user-script for Firefox's [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) extension
-// that lets you view videos on YouTube pages even if Flash and the page's JavaScript is disabled. In particular, I hope this
+// This user-script for Firefox's [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) extension
+// lets you view videos on YouTube pages even if Flash and the page's JavaScript is disabled. In particular, I hope this
 // is useful for the Tor Browser Bundle.
 
 // This script can be [installed from UserScripts.org](http://userscripts.org/scripts/show/308677)
